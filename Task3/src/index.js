836788d4447400cc25593fbf012d57d46da9bbeb
Diman-ko <dr.komarov@mail.ru>
2023-03-12 11:17:48 +0100

    feat: создание переменных ducumetn.querySelector, слушателей для переменных var.addEventListener(),  создание функций к каждому слушателю и проверка console.log('click is working')

---
+++ Task3/src/index.js
@@ -0,0 +1,49 @@
+const content = document.querySelector('#content')
+
+const minus = document.querySelector('#minus')
+const plus = document.querySelector('#plus')
+const normal = document.querySelector('#normal')
+const bold = document.querySelector('#bold')
+const italic = document.querySelector('#italic')
+
+
+minus.addEventListener('click', getMinus)
+plus.addEventListener('click', getPlus)
+normal.addEventListener('click', getNormal)
+bold.addEventListener('click', getBolt)
+italic.addEventListener('click', getItalic)
+
+
+
+function getMinus() {
+    console.log('click is working')
+
+}
+
+function getPlus() {
+    console.log('click is working')
+
+}
+
+function getNormal() {
+    console.log('click is working')
+
+}
+
+function getBolt() {
+    console.log('click is working')
+
+}
+
+function getItalic() {
+    console.log('click is working')
+
+}
+
+
+
+
+
+
+
+
