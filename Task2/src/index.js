763709bb1e93d85a42cd3f444ca1e4b6dd84aef5
Diman-ko <dr.komarov@mail.ru>
2023-03-12 07:55:11 +0100

    feat: edit index.js

--- Task2/src/index.js
+++ Task2/src/index.js
@@ -1,6 +1,12 @@
 const content = document.querySelector('#content')
 
-
+const techForm = [
+    {type: 'js', title: 'JS', dicription: 'js-text', done: false },
+    {type: 'git', title: 'GIT', dicription: 'git-text', done: false },
+    {type: 'react', title: 'REACT', dicription: 'react-text', done: false },
+    {type: 'nod', title: 'NOD', dicription: 'nod-text', done: false },
+    {type: 'job', title: 'JOB', dicription: 'job-text', done: false },
+]
 
 
 
