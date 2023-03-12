ffe7e8eaded92e2ea30e53282e7ebff6d12d5605
Diman-ko <dr.komarov@mail.ru>
2023-03-12 22:31:13 +0100

    feat: создание переменной text, id="text"слушателя  text,созданиефункций изменения стилей

--- Task3/src/index.js
+++ Task3/src/index.js
@@ -1,10 +1,10 @@
-const content = document.querySelector('#content')
 
 const minus = document.querySelector('#minus')
 const plus = document.querySelector('#plus')
 const normal = document.querySelector('#normal')
 const bold = document.querySelector('#bold')
 const italic = document.querySelector('#italic')
+const text = document.querySelector('#text')
 
 
 minus.addEventListener('click', getMinus)
@@ -14,31 +14,33 @@ bold.addEventListener('click', getBolt)
 italic.addEventListener('click', getItalic)
 
 
-
 function getMinus() {
     console.log('click is working')
- if () {
+    text.style.fontSize = '40px'
 
- }
 }
+
 
 function getPlus() {
     console.log('click is working')
-
+    text.style.fontSize = '80px'
 }
 
 function getNormal() {
     console.log('click is working')
-
+    text.style.fontWeight = 'normal'
+    text.style.fontStyle = "normal"
 }
+
 
 function getBolt() {
     console.log('click is working')
-
+    text.style.fontWeight = 'bold'
 }
 
 function getItalic() {
     console.log('click is working')
+   text.style.fontStyle = "italic"
 
 }
 
