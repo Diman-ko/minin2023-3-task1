6dc8b0cf6f8f4b44797949a194279cc116724371
Diman-ko <dr.komarov@mail.ru>
2023-03-12 08:13:55 +0100

    feat: work with title in for' tech.title tech.tipe

--- Task2/src/index.js
+++ Task2/src/index.js
@@ -1,16 +1,32 @@
 const content = document.querySelector('#content')
 
 const techForm = [
-    {type: 'js', title: 'JS', dicription: 'js-text', done: false },
-    {type: 'git', title: 'GIT', dicription: 'git-text', done: false },
-    {type: 'react', title: 'REACT', dicription: 'react-text', done: false },
-    {type: 'nod', title: 'NOD', dicription: 'nod-text', done: false },
-    {type: 'job', title: 'JOB', dicription: 'job-text', done: false },
+    {type: 'js', title: 'Выучить JavaScript', dicription: 'js-text', done: false },
+    {type: 'git', title: 'Попрактиковаться с Git', dicription: 'git-text', done: false },
+    {type: 'react', title: 'Изучить React', dicription: 'react-text', done: false },
+    {type: 'nod', title: 'Понять NodeJS', dicription: 'nod-text', done: false },
+    {type: 'job', title: 'Устроиться на работу', dicription: 'job-text', done: false },
 ]
 
+console.log(techForm)
 
+let html = ''
+ for (let i = 0; i< techForm.length; i++ ){
+    const tech = techForm[i]
+     html += `
+      <li>
+        <label data-type="${tech.type}">
+            <input type="checkbox" /> ${tech.title}
+        </label>
+      </li>
+     `
 
+ }
+ content.innerHTML = html
 
 function init() {
     
 }
+
+
+init()
