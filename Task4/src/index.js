26270c1bba101231587bbc433f091638e7b97a8f
Diman-ko <dr.komarov@mail.ru>
2023-03-15 10:23:09 +0100

    feat: add lang="en"

--- Task4/src/index.js
+++ Task4/src/index.js
@@ -3,9 +3,15 @@
 // 1) Получаем сам элемент
 const node = document.querySelector("#test");
 
+
 // 2) Получаем объект всех его стилей
 const styles = window.getComputedStyle(node);
 
 // 3) Достаем нужный стиль
 const paddingLeft = parseInt(styles.paddingLeft);
 
+console.log(styles.wordSpacing)  // 0px
+console.log(styles.textAlign)  //  start
+console.log(styles.color)  //  rgb(0, 0, 0)
+console.log(paddingLeft) //  0
+document.write('paddingLeft' + ' ' + paddingLeft)
